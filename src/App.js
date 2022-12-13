86f5d7f1b47ec481cbc8850c10dd686ef170b1cd
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-12-12 18:47:09 -0600

    imported-MemeList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js"
 import Meme from "./components/Meme.js"
+import MemeList from "./components/MemeList.js"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Meme />
+      <MemeList />
     </div>
   );
 }
