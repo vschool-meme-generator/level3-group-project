bcf3c3643dfde0c55a9bfd90a30517013592112a
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-12-13 19:32:58 -0600

    meme-setup

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,8 @@
 // Meme component handles the logic for genenrating memes using Axios and displaying 
 // them in the user interface. 
 import React from "react";
+import axios from "axios";
+import MemeList from "./MemeList.js"
 
 function Meme(){
     //creating state to render top and bottom text along with images
@@ -9,14 +11,18 @@ function Meme(){
         bottomText: "",
         randomImg: "http://i.imgflip.com/1bij.jpg"
     })
+
+    // created state for memes array from given API "https://api.imgflip.com/get_memes"
     const [allMemes, setAllMemes] = React.useState([])
 
     React.useEffect(() => {
        fetch("https://api.imgflip.com/get_memes")
         .then(res => res.json())
         .then(data => setAllMemes(data.data.memes))
+        .catch(err => console.log(err.data))
     }, [])
 
+    //onClick the meme image will change
     function getMemeImage(){
         const randomNum = Math.floor(Math.random() * allMemes.length)
         const url = allMemes[randomNum].url
@@ -26,6 +32,7 @@ function Meme(){
         }))
     }
 
+    //as input values change meme state uodates
     function handleChange(e){
         const {name, value} = e.target
         setMeme(prevState => ({
@@ -34,10 +41,18 @@ function Meme(){
         }))
     }
 
+    //state that stores created memes on submit btn
+    const [createdMeme, setCreatedMeme] = React.useState([])
+    
+    function createMeme(){
+        
+    }
+
     return(
         <main>
             <div className="form-container">
                 <input 
+                className="input"
                 type="text"
                 placeholder="Top text"
                 name="topText"
@@ -46,6 +61,7 @@ function Meme(){
                 />
 
                 <input 
+                className="input"
                 type="text"
                 placeholder="Bottom text"
                 name="bottomText"
@@ -64,6 +80,7 @@ function Meme(){
                 <h2 className="meme-top">{meme.topText}</h2>
                 <h2 className="meme-bottom">{meme.bottomText}</h2>
             </div>
+            {/* <footer className="footer">Developed by: Dasha Gaytan and Karyna Chernyak 2022</footer> */}
         </main>
     )
 }
