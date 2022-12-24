69741b8cfee3159118a8ac0bd1dee2fe988dd400
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2022-12-23 18:24:16 -0600

    added user edit btn and save changes btn

--- src/components/MemeList.js
+++ src/components/MemeList.js
@@ -3,11 +3,61 @@
 
 import React from "react";
 
+function MemeList(props){
+    const newMeme = props.newMeme
+    //save state for user created memes
+    const [newUserMeme, setNewUserMeme] = React.useState(newMeme)
 
-function MemeList(){
+    //update meme state as input values change for top and bottom texts
+    function handleChange(e){
+        e.preventDefault()
+        const {name, value} = e.target;
+        setNewUserMeme(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    //state that displays edit option onClick
+    const [display, setDisplay] = React.useState('none')
+
+    function saveEdit(e){
+        e.preventDefault()
+        props.saveMeme(newUserMeme)
+        setDisplay('none') 
+    }
+
+    function editMeme(){
+        setDisplay(true)
+    }
+
     return(
-        <>
-        </>
+        <div className="memeList-container">
+            <img className="saved-meme" src={newMeme.url} alt="saved-meme"/>
+            <h2 className="saved-top-text">{newUserMeme.topText}</h2>
+            <h2 className="saved-bottom-text">{newUserMeme.bottomText}</h2>
+
+            <div className="saved-meme-btns">
+                <button onClick={editMeme} className="edit-btn">Edit Meme</button>
+                <button onClick={props.deleteMeme} className="delete-btn">Delete Meme</button>
+            </div>
+
+            <form onSubmit={saveEdit} style = {{display: display}}>
+                <input
+                className="user-edits"
+                name="topText"
+                value={newUserMeme.topText}
+                onChange={handleChange}
+                />
+                <input
+                className="user-edits"
+                name="bottomText"
+                value={newUserMeme.bottomText}
+                onChange={handleChange}
+                />
+                <button className="save-btn">Save changes</button>
+            </form>
+        </div>
     )
 }
 
